38eb041ce476ed8d52603f9be1996580579b6d38
Cameron White <cawhite@pdx.edu>
2013-12-12 09:05:28 -0800

    Added more functions
    - get_products()
    - get_prices()
    - get_stores()

--- PyOLP/main_class.py
+++ PyOLP/main_class.py
@@ -16,6 +16,8 @@
 # along with PyGithub. If not, see <http://www.gnu.org/licenses/>.
 
 import api_exceptions
+import api_objects
+from paginated import PaginatedList
 from price import Price
 from product import Product
 from store import Store
@@ -35,23 +37,69 @@ class PyOPl:
         :param code: string
         :rtype: :class: `product.Product`
         """
-        assert isinstance(id, str), id
+        assert id is api_objects.NotSet or isinstance(id, str), id
         headers, data = self.__requester.requestJsonAndCheck(
             "/api/v1/product/" + str(id) + "/"
         )
         return Product(self.__requester, headers, data)
 
-    def get_store(self, id=None):
+    def get_products(self, code=api_objects.NotSet,
+            on_sale=api_objects.NotSet, proof=api_objects.NotSet,
+            status=api_objects.NotSet, title=api_objects.NotSet):
+        """
+        :calls: `GET /api/v1/product/`
+        :param code: string
+        :param on_sale: bool
+        :param proof: float
+        :param status: string
+        :param title: string
+        :rtype: :class: `product.Product`
+        """
+        assert code is api_objects.NotSet or isinstance(code, str), code
+        assert on_sale is api_objects.NotSet or isinstance(on_sale, bool), on_sale
+        assert proof is api_objects.NotSet or isinstance(proof, (int, float)), proof
+        assert status is api_objects.NotSet or isinstance(status, str), status
+        assert title is api_objects.NotSet or isinstance(title, str), title
+        url_parameters = dict()
+        if code is not api_objects.NotSet:
+            url_parameters["code"] = code
+        if on_sale is not api_objects.NotSet:
+            url_parameters["on_sale"] = on_sale
+        if proof is not api_objects.NotSet:
+            url_parameters["proof"] = proof
+        if status is not api_objects.NotSet:
+            url_parameters["status"] = status
+        if title is not api_objects.NotSet:
+            url_parameters["title"] = title
+        return PaginatedList(
+            Product,
+            self.__requester,
+            '/api/v1/product/',
+            url_parameters
+        )
+
+    def get_store(self, id=api_objects.NotSet):
         """
         :calls: `GET /api/v1/store/`
         :param id: string
         :rtype: :class: `store.Store`
         """
-        assert isinstance(id, str), id
+        assert id is api_objects.NotSet or isinstance(id, str), id
         headers, data = self.__requester.requestJsonAndCheck(
             "/api/v1/store/" + str(id) + "/"
         )
         return Store(self.__requester, headers, data)
+
+    def get_stores(self):
+        """
+        :calls: `GET /api/v1/store/`
+        :rtype: :class: `store.Store`
+        """
+        return PaginatedList(
+            Store,
+            self.__requester,
+            '/api/v1/store/'
+        )
 
     def get_price(self, id=None):
         """
@@ -59,9 +107,19 @@ class PyOPl:
         :param id: string
         :rtype: :class: `price.Price`
         """
-        assert isinstance(id, str), id
+        assert id is api_objects.NotSet or isinstance(id, str), id
         headers, data = self.__requester.requestJsonAndCheck(
             "/api/v1/price/" + str(id) + "/"
         )
         return Price(self.__requester, headers, data)
 
+    def get_prices(self):
+        """
+        :calls: `GET /api/v1/price/`
+        :rtype: :class: `price.Price`
+        """
+        return PaginatedList(
+            Price,
+            self.__requester,
+            '/api/v1/price/'
+        )
